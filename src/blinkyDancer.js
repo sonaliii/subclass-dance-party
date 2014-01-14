203a07c07e2383b159de6ad7b768048d83d6d072
Hack Reactor Students <sona8242@gmail.com>
2014-01-13 17:16:27 -0800

    Converts dancer and blinkyDancer to pseudoclassical implementation

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,28 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+// var makeBlinkyDancer = function(top, left, timeBetweenSteps){
+//   var blinkyDancer =  new Dancer(top, left, timeBetweenSteps);
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-  var oldStep = blinkyDancer.step;
+//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+//   // so we must keep a copy of the old version of this function
+//   var oldStep = blinkyDancer.step;
 
-  blinkyDancer.step = function(){
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+//   blinkyDancer.step = function(){
+//     // call the old version of step at the beginning of any call to this new version of step
+//     oldStep.apply(this);
 
-    /* toggle() is a jQuery method to show/hide the <span> tag.
-     * See http://api.jquery.com/category/effects/ for this and
-     * other effects you can use on a jQuery-wrapped html tag. */
-    blinkyDancer.$node.toggle();
-  };
+//     blinkyDancer.$node.toggle();
+//   };
 
-  return blinkyDancer;
+//   return blinkyDancer;
+// };
+
+var BlinkyDancer = function(top, left, timeBetweenSteps){
+  Dancer.apply(this, arguments);
+};
+
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+//BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+BlinkyDancer.prototype.step = function(){
+   Dancer.prototype.step.apply(this);
+   this.$node.toggle();
 };
